da8a92c4c108f327c38a1fb8e16738dacbaaa6ca
iaservicos <iaservicos@positivo.com.br>
2026-04-20 11:29:22 -0300

    fix: Adicionar buscarTipoEquipamento de volta

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: 'terser',
-  },
-})
+rollupOptions: {
+  external: ['xlsx'],
+  output: {
+    globals: {
+      xlsx: 'XLSX'
+    }
+  }
+}
